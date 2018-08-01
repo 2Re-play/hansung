9270a758802abf58488137c71548e756bd9b624c
jaehyun <wogusduddnjs@gmail.com>
2018-08-01 14:28:41 +0900

    줄 바꿈 수정

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -6,12 +6,12 @@ exports.message = async (req, res) => {
   const { content } = req.body
   try {
     const result = await message_logic.selectmessage(content)
-    if (content === '학생식당 메뉴') storename_respondJson('식당을 선택해주세요.', result, res, 200) 
-    else if (content === '열람실') storename_respondJson('열람실 현황입니다.', result, res, 200) 
-    else if (content === '버스 도착시간') storename_respondJson('버스 예상 도착 시간입니다.', result, res, 200) 
-    else if (content === '학교날씨') storename_respondJson('한성대학교 날씨입니다.', result, res, 200) 
-    else if (content === 'ROll&Noodles') menulist_respondJson(`${content}의 메뉴입니다.`, result, res, 200)
-    else if (content === 'The bab') menulist_respondJson(`${content}의 메뉴입니다.`, result, res, 200)
+    if (content === '학생식당 메뉴') storename_respondJson('식당을 선택해주세요.\n', result, res, 200) 
+    else if (content === '열람실') storename_respondJson('열람실 현황입니다.\n', result, res, 200) 
+    else if (content === '버스 도착시간') storename_respondJson('버스 예상 도착 시간입니다.\n', result, res, 200) 
+    else if (content === '학교날씨') storename_respondJson('한성대학교 날씨입니다.\n', result, res, 200) 
+    else if (content === 'ROll&Noodles') menulist_respondJson(`${content}의 메뉴입니다.\n`, result, res, 200)
+    else if (content === 'The bab') menulist_respondJson(`${content}의 메뉴입니다.\n`, result, res, 200)
   } catch (e) {
     respondOnError(e.message, res, 500)
     console.log(e.message)
