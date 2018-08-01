a98b0ec5df119f1e440c3529fb5808c2d0ac404a
jaehyun <wogusduddnjs@gmail.com>
2018-08-01 14:57:12 +0900

    처음으로 추가

--- src/logic/messageLogic.js
+++ src/logic/messageLogic.js
@@ -1,5 +1,6 @@
 const common_buttons = require('../models/common_buttons')
 const dbConnection = require('../lib/dbConnection')
+const main_buttons = require('../models/main_buttons')
 
 // 최상위 버튼 선택 
 exports.selectmessage = async (content, err) => {
@@ -12,17 +13,20 @@ exports.selectmessage = async (content, err) => {
     else if (content === '학교날씨') result = await common_buttons.storename(connection)
     else if (content === 'ROll&Noodles') result = await menu_list(content)
     else if (content === 'The bab') result = await menu_list(content)
+    else if (content === '처음으로') result = await main_buttons.main(connection)
     else if (err) reject(err)
     resolve(result)
     connection.release()
   })
   return result
 }
-// 학생식당 메뉴 디비
+// 학생식당  디비
 const selectstore = async () => {
   const connection = await dbConnection()
   try {
     const result = await common_buttons.storename(connection)
+    result.push('처음으로')
+    console.log(result)
     return result
   } catch (e) {
     console.log(e.message)
@@ -36,6 +40,7 @@ const menu_list = async (content) => {
   const connection = await dbConnection()
   try {
     const result = await common_buttons.menuname(connection, content)
+    result.push('처음으로')
     return result
   } catch (e) {
     console.log(e.message)
