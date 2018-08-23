b74bde45bfd1230bfa3163c8dd8e6e19eaf89690
jaehyun <wogusduddnjs@gmail.com>
2018-08-23 21:55:33 +0900

    한성대 미세먼지 복구

--- src/lib/fineDust.js
+++ src/lib/fineDust.js
@@ -1,4 +1,6 @@
-const request = require('request')
+const request = require('request-promise')
+const qs = require('querystring')
+const opendata = require('../../config/serviceKey')
 /*
     미세먼지 기준
     0~30 좋음
@@ -10,19 +12,36 @@ const request = require('request')
 exports.air = async () => {
   let info
   let data
-  const url = 'http://113.198.91.9/domianweb/roomview5.asp?room_no=1' // 제 1열람실(3층)
+  const str = '서울'
+  const encodedStr = qs.escape(str)
+  const url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst'
+  const queryParams = `?sidoName=${encodedStr}&searchCondition=DAILY&pageNo=17&numOfRows=1&ServiceKey=${opendata.serviceKey}&_returnType=json` /* 정류소고유번호 */
   const option = {
-    uri: url,
+    uri: url + queryParams,
     method: 'GET',
   }
   await new Promise(async (resolve, reject) => {
-    request(option, (e, response, body) => {
-      console.log('body', body)
-      if (e) {
-        reject(e)
-      } else {
-        resolve(info)
-      }
+    await request(option, async (e, response, body) => {
+      request(option, function (e, response, body) {
+        data = body
+        data = JSON.parse(body)
+        const time = data.list[0].dataTime
+        const dust = data.list[0].pm10Value
+        let dustStatus
+
+        if (dust < 30) dustStatus = '좋음😆'
+        else if (dust > 30 && dust < 80) dustStatus = '보통🙂'
+        else if (dust > 80 && dust < 150) dustStatus = '나쁨😫'
+        else if (dust > 150) dustStatus = '매우나쁨😡'
+        info = {
+          time,
+          dust,
+          dustStatus,
+        }
+        console.log(info)
+        if (e) reject(e)
+        else resolve(info)
+      })
     })
   })
   return info
