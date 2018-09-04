12b3fe10959098371791ee521b6aadda0ae3810e
jaehyun <wogusduddnjs@gmail.com>
2018-09-04 23:37:11 +0900

    코드 수정

--- src/lib/fineDust.js
+++ src/lib/fineDust.js
@@ -21,27 +21,25 @@ exports.air = async () => {
     method: 'GET',
   }
   await new Promise(async (resolve, reject) => {
-    await request(option, async (e, response, body) => {
-      request(option, function (e, response, body) {
-        data = body
-        data = JSON.parse(body)
-        const time = data.list[0].dataTime
-        const dust = data.list[0].pm10Value
-        let dustStatus
+    request(option, (e, response, body) => {
+      data = body
+      data = JSON.parse(body)
+      const time = data.list[0].dataTime
+      const dust = data.list[0].pm10Value
+      let dustStatus
 
-        if (dust < 30) dustStatus = '좋음😆'
-        else if (dust > 30 && dust < 80) dustStatus = '보통🙂'
-        else if (dust > 80 && dust < 150) dustStatus = '나쁨😫'
-        else if (dust > 150) dustStatus = '매우나쁨😡'
-        info = {
-          time,
-          dust,
-          dustStatus,
-        }
-        console.log(info)
-        if (e) reject(e)
-        else resolve(info)
-      })
+      if (dust < 30) dustStatus = '좋음😆'
+      else if (dust > 30 && dust < 80) dustStatus = '보통🙂'
+      else if (dust > 80 && dust < 150) dustStatus = '나쁨😫'
+      else if (dust > 150) dustStatus = '매우나쁨😡'
+      info = {
+        time,
+        dust,
+        dustStatus,
+      }
+      console.log(info)
+      if (e) reject(e)
+      else resolve(info)
     })
   })
   return info
